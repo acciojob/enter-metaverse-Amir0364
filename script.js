011fe7e0b7d75a38225cfa3313a10cd6dd7dd49d
Mohd Amir <140543590+Amir0364@users.noreply.github.com>
2024-07-06 13:53:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 //your JS code here. If required.
-var 
+var statusElement = document.getElementById('status');
+var enterButton = document.getElementById('enterBtn');
+enterButton.addEventListener('click', function(){
+	var newElement = document.createElement('h1');
+	newElement.innerText = "Entered Metaverse";
+		statusElement.parentNode.replaceChild(newElement, statusElement);
+});
